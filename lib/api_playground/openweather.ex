3c0ad6f60c17675fb8000866edd152edac02f996
Nicholas Moen <arcanemachine@tutanota.com>
2023-10-22 04:24:05 -0600

    Support choice of HTTP clients when making a request

--- lib/api_playground/openweather.ex
+++ lib/api_playground/openweather.ex
@@ -17,7 +17,7 @@ defmodule ApiPlayground.Openweather do
   @doc """
   Makes a request to the OpenWeather API server for the current weather. Returns a Req response.
 
-  Parameters:
+  URL options (appended to the API URL):
 
     - `appid`: The OpenWeather API key used to make the request (default: nil)
       - If the OPENWEATHER_API_KEY environment variable is present, then that value will be used.
@@ -31,6 +31,12 @@ defmodule ApiPlayground.Openweather do
         parsed correctly.
 
     - `units`: The units of measurement. (default: "standard")
+
+  Non-URL options (stripped before generating the API URL query string):
+
+    - `http_client`: The HTTP client used to make the request. (default: "req")
+      - This option exists only as a form of documentation in regards to how to use each client.
+      - Must be one of: "req"
   """
   def current(opts \\ []) do
     # merge default opts with opts passed by user
@@ -38,7 +44,8 @@ defmodule ApiPlayground.Openweather do
       appid: System.get_env("OPENWEATHER_API_KEY"),
       q: "Edmonton",
       units: "metric",
-      mode: "json"
+      mode: "json",
+      http_client: "req"
     ]
 
     opts = Keyword.merge(default_opts, opts)
@@ -46,10 +53,16 @@ defmodule ApiPlayground.Openweather do
     # ensure that we have an API key before continuing
     _ensure_api_key(opts[:appid])
 
+    # strip non-URL options before generating the query string for the API request URL
+    {http_client, opts} = opts |> Keyword.pop(:http_client)
+
     # send a request to the API server and return the response
     url = "#{@current_weather_endpoint}?#{URI.encode_query(opts)}"
 
-    Req.get!(url)
+    case http_client do
+      "req" -> Req.get!(url)
+      _ -> throw("Option 'http_client' must be one of: \"req\"")
+    end
   end
 
   @doc "Returns the current temperature from an OpenWeather API response."
